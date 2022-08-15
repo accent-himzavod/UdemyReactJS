c29147351c5b30f220e85e882c56c700e790783a
Alex <banul.sx@himzavod.com.ua>
2022-08-15 15:44:12 +0300

    Exercise 11 \task 2 - done

--- js/exercise11_Obj.js
+++ js/exercise11_Obj.js
@@ -7,13 +7,32 @@ const newArray = oldArray.slice(); //clone array by value, (Object.assign())
 const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
 function showFamily(arr) {
-    return `Семья состоит из: ${[...arr].join(' ')}`;
+    if (arr.length === 0) {
+        return `Семья пуста`;
+    } else {
+        return `Семья состоит из: ${[...arr].join(' ')}`;
+    }    
 }
-
 console.log(showFamily(family));
 
 const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
 
 function standardizeStrings(arr) {
-    
+    console.log([...arr].join('\n').toLocaleLowerCase());
 }
+standardizeStrings(favoriteCities);
+
+//teacher
+// function showFamily(arr) {
+//     let str = '';
+//     arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+//     arr.forEach(member => {
+//         str += `${member} `
+//     });
+//     return str;
+// }
+// function standardizeStrings(arr) {
+//     arr.forEach(city => {
+//         console.log(city.toLowerCase())
+//     })
+// }
