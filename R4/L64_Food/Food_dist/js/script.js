3cab479a3dac03c08208521621b5accb79f6593f
Alex <banul.sx@himzavod.com.ua>
2022-09-05 19:04:13 +0300

    Food - done

---
+++ R4/L64_Food/Food_dist/js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+window.addEventListener('DOMContentLoaded',() => {
+    const   tabsParent = document.querySelector('.tabheader__items'),
+            tabs = tabsParent.querySelectorAll('.tabheader__item'),
+            tabsContent = document.querySelectorAll('.tabcontent');
+            
+    
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active')
+        })
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item,i) => {
+                if (item == target) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            })
+        }
+    })
+    hideTabContent();
+    showTabContent(0);
+
+
+});
