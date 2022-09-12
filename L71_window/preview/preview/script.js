235f1421e92cdea7c559c19e2eed00d935c56f54
Alex <banul.sx@himzavod.com.ua>
2022-09-12 12:14:21 +0300

    metrics - done

--- L71_window/preview/preview/script.js
+++ L71_window/preview/preview/script.js
@@ -1,2 +1,40 @@
 'use strict';
 
+const   box = document.querySelector('.box'),
+        btn = document.querySelector('button');
+
+const   heightCl = box.clientHeight,
+        widthCl = box.clientWidth;
+
+const   heightOff = box.offsetHeight,
+        widthOff = box.offsetWidth;
+
+const   heightScrl = box.scrollHeight,
+        widthScrl = box.scrollWidth;
+
+console.log(widthCl, heightCl);
+
+const style = window.getComputedStyle(box);
+const boxHeight = window.getComputedStyle(box).height;
+            
+btn.addEventListener('click', () => {
+    console.log(box.offsetHeight +" "+ box.scrollHeight);
+    console.log(box.clientHeight - box.scrollHeight);
+    if (box.offsetHeight == box.scrollHeight){
+        box.style.height = boxHeight;
+    } else {
+        box.style.height = box.scrollHeight + 'px';
+    }
+    console.log(box.scrollTop);
+    console.log(box.getBoundingClientRect());
+    console.log(box.getBoundingClientRect().top);
+    console.log(style.height);
+    console.log(document.documentElement.scrollTop);
+    document.documentElement.scrollTop = 100;
+    window.scrollTo(0,20);
+    window.scrollBy(0,20);
+
+    
+});
+
+
