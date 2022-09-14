505cb614460e63e4694200626cb80ac5de69693a
Alex <banul.sx@himzavod.com.ua>
2022-09-14 20:11:45 +0300

    HotLine

--- R4/L64_Food/Food_dist/js/script.js
+++ R4/L64_Food/Food_dist/js/script.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded',() => {
             tabs = tabsParent.querySelectorAll('.tabheader__item'),
             tabsContent = document.querySelectorAll('.tabcontent');
             
-    
+    //TABs
     function hideTabContent() {
         tabsContent.forEach(item => {
             item.style.display = 'none';
@@ -157,4 +157,19 @@ window.addEventListener('DOMContentLoaded',() => {
     }
 
     window.addEventListener('scroll', openModalByScrol);
+
+
+    const names = document.querySelectorAll(".list-item__title");
+    const desc = document.querySelectorAll(".list-item__specifications-text");
+    const prices = document.querySelectorAll(".price__value");
+    const pricesDesc = document.querySelectorAll(".text-sm");
+    let resultTxt = "";
+    console.log(names.forEach((prodName, i)=>{resultTxt += `${prodName.textContent.split(' ').join(' ').split('\n').join('').trim()};${desc[i].textContent.split(' ').join(' ').split('\n').join('').trim()};${prices[i].textContent};${pricesDesc[i].textContent}\n`;}));
+
+    const arr = [document.querySelectorAll(".list-item__title"),document.querySelectorAll(".list-item__specifications-text"),document.querySelectorAll(".price__value"),document.querySelectorAll(".text-sm")];
+
+    const getHot = (arr)=>{let rt = ''; arr[0].forEach((prodName, i)=>{rt += `${arr[0][i].textContent.split(' ').join(' ').split('\n').join('').trim()};${arr[1][i].textContent.split(' ').join(' ').split('\n').join('').trim()};${arr[2][i].textContent.split(' ').join(' ').split('\n').join('').trim()} \n`;}); return rt;};getHot([document.querySelectorAll(".list-item__title"),document.querySelectorAll(".list-item__specifications-text"),document.querySelectorAll(".price__value"),document.querySelectorAll(".text-sm")])
+
+
+    resultTxt = (arr)=>{let rt; arr[0].forEach((prodName, i)=>{rt += `${arr[0][i].textContent}`;}); return rt}
 });
