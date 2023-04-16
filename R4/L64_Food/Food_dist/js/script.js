c41c2cd6b4bce37a1d1bd1043c6239d46c5689e5
banul.sx <banul.sx@gmail.com>
2023-04-17 00:24:45 +0300

    L84_OpenSrv_done

--- R4/L64_Food/Food_dist/js/script.js
+++ R4/L64_Food/Food_dist/js/script.js
@@ -234,3 +234,79 @@ window.addEventListener('DOMContentLoaded',() => {
         "menu__item"
         ).render();    
 });
+
+//Forms
+//1-FormData
+//2-XMLHttpRequest
+const forms = document.querySelectorAll('form');
+
+forms.forEach(form => {
+    postJsonData(form);
+});
+
+const message = {
+    success: 'Success!',
+    loading: 'loading...',
+    failure: 'error!'
+}
+
+function postFormData(form){
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        form.append(statusMessage);        
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        //  request.setRequestHeader('Content-Type', 'multipart/form-data'); //Don't use when XMLHttpRequest + FormData
+        const formData = new FormData(form);
+        request.send(formData);
+        request.addEventListener('load', ()=>{
+            if (request.status === 200){
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+                form.reset();
+                setTimeout(()=>{
+                    statusMessage.remove();
+                },2000);
+            } else {
+                statusMessage.textContent = message.failure;
+            }
+        });        
+    });    
+}
+
+function postJsonData(form){
+    form.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        statusMessage.textContent = message.loading;
+        form.append(statusMessage);
+        
+        const request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        //  request.setRequestHeader('Content-Type', 'multipart/form-data'); //Don't use when XMLHttpRequest + FormData
+        request.setRequestHeader('Content-Type', 'application/json'); //Don't use when XMLHttpRequest + FormData
+        const formData = new FormData(form);
+        const obj = {};
+        formData.forEach(function(value,key){
+            obj[key] = value;
+        });
+        const json = JSON.stringify(obj);
+        request.send(json);
+        request.addEventListener('load', ()=>{
+            if (request.status === 200){
+                console.log(request.response);
+                statusMessage.textContent = message.success;
+                form.reset();
+                setTimeout(()=>{
+                    statusMessage.remove();
+                },2000);
+            } else {
+                statusMessage.textContent = message.failure;
+            }
+        });        
+    });    
+}
