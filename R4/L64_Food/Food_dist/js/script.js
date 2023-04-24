2766ba40c2e1b8ba37229b4058111078fb773d21
Alex <banul.sx@himzavod.com.ua>
2023-04-24 19:33:30 +0300

    L87_fetch - start :

--- R4/L64_Food/Food_dist/js/script.js
+++ R4/L64_Food/Food_dist/js/script.js
@@ -342,6 +342,20 @@ window.addEventListener('DOMContentLoaded',() => {
     
     }
 
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(resolve => resolve.json())
+    .then(json => console.log(json));
+    
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: 'POST',
+        body: JSON.stringify({name: "Oleg", pass: "}Pdiufs"}),
+        headers: {
+            "Content-type": "apliacation/json"
+        }
+    })
+    .then(resolve => resolve.json())
+    .then(json => console.log(json));
+
 });
 
 
