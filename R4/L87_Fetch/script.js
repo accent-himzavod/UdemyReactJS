bea62344988345816accf44d572a589dbb2a8a1f
Alex <banul.sx@himzavod.com.ua>
2023-04-28 16:55:37 +0300

    Added package.json

--- R4/L87_Fetch/script.js
+++ R4/L87_Fetch/script.js
@@ -1,6 +1,22 @@
 'use strict';
 
-
+//JSON-PLACEHOLDER
 fetch('https://jsonplaceholder.typicode.com/posts/1')
+	.then(resolve => resolve.json())
+	.then(json => console.log(json));
+
+
+//GET
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+	.then(resolve => resolve.json())
+	.then(json => console.log(json));
+//POST
+fetch('https://jsonplaceholder.typicode.com/posts',{
+	method: 'POST',
+	body: JSON.stringify({name: 'Oleg', pass: '}Pdiufs'}),
+	headers: {
+		'Content-type': 'apliacation/json'
+	}
+})
 	.then(resolve => resolve.json())
 	.then(json => console.log(json));
